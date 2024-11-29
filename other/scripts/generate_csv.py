4b57b52784b06800475fea09da03656834e6f3c0
LS0205 <lukas.springer@avl.com>
2024-11-29 14:49:40 +0100

    finished the python version

--- other/scripts/generate_csv.py
+++ other/scripts/generate_csv.py
@@ -3,13 +3,13 @@ import re
 import pandas as pd
 
 
-def find_markdown_files(directory, keyword):
+def find_markdown_files(directory, excluded_dirs, keyword):
     markdown_files = []
     for root, dirs, files in os.walk(directory):
+        dirs[:] = [d for d in dirs if d not in excluded_dirs]
         for file in files:
             if file.endswith(".md") and keyword in file:
                 markdown_files.append(os.path.join(root, file))
-                # print(f"{os.path.join(root, file)}\n ")
     return markdown_files
 
 
@@ -18,7 +18,7 @@ def write_to_csv(df, output_csv):
 
 
 def extract_headings_and_content(file_path):
-    name_pattern = re.compile(r"(# ([a-zA-Z +\-\ \/(\)]*)\s*## BB Tag)")
+    name_pattern = re.compile(r"# ([a-zA-Z0-9, +\-\ \/(\)]*)\s*## BB Tag")
     heading_pattern = re.compile(r"## ([a-zA-Z +\-\/\(\)]*)")
 
     with open(file_path, "r", encoding="utf-8") as file:
@@ -26,16 +26,16 @@ def extract_headings_and_content(file_path):
 
     content = re.sub(r"<!--.*?-->", "", content, flags=re.DOTALL)
     headings = heading_pattern.findall(content)
+    # headings = []
     names = name_pattern.findall(content)
     print(names)
 
-    heading_contents = {}
+    heading_contents = {"BB Name": names[0]}
 
     for i, heading in enumerate(headings):
         start = content.find(heading) + len(heading)
         end = content.find(headings[i + 1]) if i + 1 < len(headings) else len(content)
         heading_content = content[start : end - 3].replace("\n", " ").strip()
-        # content = content.replace("\n", " ")
         heading_contents[heading] = heading_content
     return heading_contents
 
@@ -44,6 +44,7 @@ def create_df(markdown_files):
     file_contents = []
 
     for file in markdown_files:
+        print(file)
         heading_content = extract_headings_and_content(file)
         file_contents.append(heading_content)
 
@@ -52,7 +53,7 @@ def create_df(markdown_files):
 
 
 def main():
-    directory_to_traverse = "./"
+    directory_to_traverse = "."
     keyword = "BB"
     output_csv = "./other/utils/bb_library.csv"
 
@@ -82,8 +83,10 @@ def main():
     #     "System Context",
     # ]
 
-    markdown_files = find_markdown_files(directory_to_traverse, keyword)
-    markdown_files = markdown_files[0]
+    excluded_dirs = [".github", "other", "scripts", "utils"]
+
+    markdown_files = find_markdown_files(directory_to_traverse, excluded_dirs, keyword)
+    # markdown_files = [markdown_files[0]]
     # print(markdown_file)
     df = create_df(markdown_files)
 
